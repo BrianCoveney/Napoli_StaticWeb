c49b87214d7b0dad9ac1f8a0157413d5af95af8c
BrianCoveney <coveneybrian@gmail.com>
2015-04-27 18:17:46 +0100

    added seperate list for phone screen, drop down menu

--- js/myjquery.js
+++ js/myjquery.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		//Function for hide/show menu on small screens
 	    $( ".hamburger-menu" ).click(function() 
 	    {
-	        $(".hamburger-menu").next().slideToggle(1000)
+	        $(".hamburger-menu:not('first-child')").next().slideToggle(1000)
 	    }
     );
 

8b4f24a24194d9615e576f23cb06f459ad3fd079
BrianCoveney <brian.coveney@mycit.ie>
2015-05-12 23:13:07 +0100

    change to nav for tablet screens. 'Product' pages additions

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,6 +79,6 @@ module.exports = function(grunt) {
 
   // Default task(s).
   grunt.registerTask('default', ['uglify','compass', 
-    'imagemin', 'watch']);
+    'imagemin', 'watch', 'sass']);
 
 };

cd2f12621353fd6fcf5f43cf4cd21a1d7607e1fb
BrianCoveney <brian.coveney@mycit.ie>
2015-05-13 11:51:47 +0100

    Major content change

--- js/global.js
+++ js/global.js
@@ -28,7 +28,8 @@ function initialize() {
       var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
 
       var contentString = 
-      'Napoli Italian Delicatessen<br/>5 Castle Terrace,<br/>Monkstown, Cork, Ireland.<br/>tel: 021 4841002';
+      'Napoli Italian Delicatessen<br/>5 Castle Terrace,<br/>Monkstown, Cork, Ireland.<br/>tel: 021 4841002<br>
+        <a href="https://www.google.ie/maps/@51.847037,-8.334443,3a,75y,123.28h,80.6t/data=!3m4!1e1!3m2!1s53GwbUUlj8h8210i1DPBPQ!2e0!6m1!1e1">Street View</a>';
 
       var infowindow = new google.maps.InfoWindow({
        content: contentString,

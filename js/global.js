3b3a4e16933b704c373b231d404dc554b2108da4
BrianCoveney <coveneybrian@gmail.com>
2016-06-11 14:38:18 +0100

    Fixed flashing image on page load, by changing bg-color and moving css & js links from footer back into the head. Updated package.jason -- imagemin to 1.0.1. Also fixed broken footer layout in safari mobile view.

--- js/global.js
+++ js/global.js
@@ -25,27 +25,23 @@ function initialize() {
         zoom: 15,
         center: myLatLng,
       }
-      var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
+      var map = new google.maps.Map(
+        document.getElementById('map-canvas'), myOptions);
 
-      var contentString = 
-      'Napoli Italian Delicatessen<br/>5 Castle Terrace,<br/>Monkstown, Cork, Ireland.<br/>tel: 021 4841002<br>
-        <a href="https://www.google.ie/maps/@51.847037,-8.334443,3a,75y,123.28h,80.6t/data=!3m4!1e1!3m2!1s53GwbUUlj8h8210i1DPBPQ!2e0!6m1!1e1">Street View</a>';
+      var panoramaOptions = {
+        position: myLatLng,
+        pov: {
+          heading: 34,
+          pitch: 10
+        }
+      };
 
-      var infowindow = new google.maps.InfoWindow({
-       content: contentString,
-       maxWidth: 200
-     });
 
-  //Marker
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
+       var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+  map.setStreetView(panorama);
 
-  });
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
+
 
 }
 /**** Google Map listener ****/
-// google.maps.event.addDomListener(window, 'load', initialise);
+google.maps.event.addDomListener(window, 'load', initialise);
